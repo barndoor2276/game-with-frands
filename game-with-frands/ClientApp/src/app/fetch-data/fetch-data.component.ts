a421336809d549842c9b5b676ac75055f118bdca
Bernard Bergan <bernard@berganmail.com>
2020-04-19 18:53:14 -0500

    test hook again

--- game-with-frands/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ game-with-frands/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,7 +9,7 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    var thing = 0;
+    var thing = 1;
     http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
